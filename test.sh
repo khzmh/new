4bb64deadec2eca23ff24870a3002f2a6db3a6c4
khzmh <sitikhojaimah@gmail.com>
2025-05-24 17:51:33 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-./xcb -P stratum1+tcp://cb23d6d8557e776f5ff9ab6a7fb7f59a3d385245fa7a.rstudio@sg.catchthatrabbit.com:8008
+./coreminer -P stratum1+tcp://cb23d6d8557e776f5ff9ab6a7fb7f59a3d385245fa7a.rstudio@sg.catchthatrabbit.com:8008
